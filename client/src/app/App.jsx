885dcb23662a33675abfe18beb9024bce595c47a
Alex Ashley <alex@ashley-family.net>
2020-04-29 20:11:48 +0100

    add password reset page and track listing page

--- client/src/app/App.jsx
+++ client/src/app/App.jsx
@@ -4,9 +4,9 @@ import { Switch, Route } from 'react-router-dom';
 
 import { NavPanel } from '../components/NavPanel';
 import routes from '../routes';
-import { IndexPage } from '../games/components';
+import { IndexPage, TrackListingPage } from '../games/components';
 import { ChooseTicketsPage, PlayGamePage, ManageGamePage } from '../tickets/components';
-import { LoginPage, LogoutPage } from '../user/components';
+import { LoginPage, LogoutPage, PasswordResetPage } from '../user/components';
 import { RegisterPage } from '../user/components/RegisterPage';
 import { history } from './store';
 
@@ -21,7 +21,9 @@ function App() {
           <Route exact path={routes.login} component={LoginPage} />
           <Route exact path={routes.logout} component={LogoutPage} />
           <Route exact path={routes.register} component={RegisterPage} />
+          <Route exact path={routes.passwordReset} component={PasswordResetPage} />
           <Route exact path={routes.game} component={ChooseTicketsPage} />
+          <Route exact path={routes.trackListing} component={TrackListingPage} />
           <Route exact path={routes.manage} component={ManageGamePage} />
           <Route exact path={routes.play} component={PlayGamePage} />
           <Route path={routes.index} component={IndexPage} />
---
+++ client/src/games/components/TrackListingPage.jsx
@@ -0,0 +1,74 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+import { initialState } from '../../app/initialState';
+import { fetchUserIfNeeded, userIsLoggedIn } from '../../user/userSlice';
+import { fetchGamesIfNeeded, fetchDetailIfNeeded } from '../gamesSlice';
+import { LoginDialog } from '../../user/components/LoginDialog';
+import { TrackListing } from './TrackListing';
+
+import '../styles/games.scss';
+
+class TrackListingPage extends React.Component {
+  static propTypes = {
+    dispatch: PropTypes.func.isRequired,
+    loggedIn: PropTypes.bool.isRequired,
+    game: PropTypes.object.isRequired,
+    user: PropTypes.object.isRequired,
+  };
+
+  componentDidMount() {
+    const { dispatch, gamePk } = this.props;
+    dispatch(fetchUserIfNeeded());
+    dispatch(fetchGamesIfNeeded());
+    dispatch(fetchDetailIfNeeded(gamePk));
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const { dispatch, gamePk } = this.props;
+    if (prevProps.user.pk !== this.props.user.pk) {
+      dispatch(fetchGamesIfNeeded());
+    }
+    if (prevProps.game.placeholder !== this.props.game.placeholder) {
+      dispatch(fetchDetailIfNeeded(gamePk));
+    }
+  }
+
+  render() {
+    const { game, loggedIn } = this.props;
+    return (
+      <div id="track-listing-page" className={loggedIn ? '' : 'modal-open'}  >
+        <TrackListing game={game} />
+        {!loggedIn && <LoginDialog backdrop dispatch={this.props.dispatch} onSuccess={() => null} />}
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state, ownProps) => {
+  state = state || initialState;
+  const { user } = state;
+  const { gamePk } = ownProps.match.params;
+  let game = state.games.games[gamePk];
+  if (game === undefined) {
+    game = {
+      title: '',
+      pk: -1,
+      tracks: [],
+      placeholder: true,
+    };
+  }
+  return {
+    loggedIn: userIsLoggedIn(state),
+    game,
+    gamePk,
+    user,
+  };
+};
+
+TrackListingPage = connect(mapStateToProps)(TrackListingPage);
+
+export {
+  TrackListingPage
+};
