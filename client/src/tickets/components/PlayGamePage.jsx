e6d8f7344663868abdce64950c3100cb16b9700f
Alex Ashley <alex@ashley-family.net>
2020-05-20 18:38:27 +0100

    fix download PDF

--- client/src/tickets/components/PlayGamePage.jsx
+++ client/src/tickets/components/PlayGamePage.jsx
@@ -18,6 +18,41 @@ import { getUser } from '../../user/userSelectors';
 /* data */
 import { initialState } from '../../app/initialState';
 
+function deepCompareObjects(a, b) {
+  if (typeof (a) !== typeof (b)) {
+    return false;
+  }
+  if (!a && b) {
+    return false;
+  }
+  if (a && !b) {
+    return false;
+  }
+  for (let k in a) {
+    if (a[k] !== b[k]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function deepCompareArrays(a, b) {
+  if (a.length !== b.length) {
+    return false;
+  }
+  for (let i = 0; i < a.length; ++i) {
+    if (typeof (a[i]) !== typeof (b[i])) {
+      return false;
+    }
+    if (typeof (a[i]) === 'object' && !deepCompareObjects(a[i], b[i])) {
+      return false;
+    } else if (a[i] !== b[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
 class PlayGamePage extends React.Component {
   static propTypes = {
     tickets: PropTypes.array.isRequired,
@@ -27,7 +62,7 @@ class PlayGamePage extends React.Component {
 
   componentDidMount() {
     const { dispatch, game, tickets } = this.props;
-    dispatch(fetchUserIfNeeded())
+    dispatch(fetchUserIfNeeded());
     if (game.pk > 0) {
       dispatch(fetchTicketsIfNeeded(game.pk));
       tickets.forEach(ticket => dispatch(fetchTicketDetailIfNeeded(game.pk, ticket.pk)));
@@ -42,7 +77,7 @@ class PlayGamePage extends React.Component {
     if (game.pk > 0 && game.pk !== prevProps.game.pk) {
       dispatch(fetchTicketsIfNeeded(game.pk));
     }
-    if (game.pk > 0 && tickets.length !== prevProps.tickets.length) {
+    if (game.pk > 0 && !deepCompareArrays(tickets, prevProps.tickets)) {
       tickets.forEach(ticket => dispatch(fetchTicketDetailIfNeeded(game.pk, ticket.pk)));
     }
   }
@@ -50,15 +85,15 @@ class PlayGamePage extends React.Component {
   setChecked = (values) => {
     const { dispatch } = this.props;
     dispatch(setChecked(values));
-  }
+  };
 
   render() {
-    const { game, tickets } = this.props;
+    const { dispatch, game, tickets } = this.props;
     return (
       <div className="card-list">
         {tickets.length === 0 && <h2 className="warning">You need to choose a ticket to be able to play!</h2>}
         {tickets.map((ticket, idx) => <BingoTicket key={idx} ticket={ticket} game={game}
-          setChecked={this.setChecked} download />)}
+          setChecked={this.setChecked} dispatch={dispatch} download />)}
       </div>
     );
   }
