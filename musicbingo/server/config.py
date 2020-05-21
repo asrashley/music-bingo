a6e05e6f90da0c8cbf2fbc82b2c32efeba9f2fc4
Alex Ashley <alex@ashley-family.net>
2020-05-21 18:55:21 +0100

    fix loading of production app config

--- musicbingo/server/config.py
+++ musicbingo/server/config.py
@@ -1,15 +1,14 @@
 from datetime import timedelta
 import os
 
-AppConfig = {
-    'DEBUG': False,
-    'SESSION_COOKIE_SECURE': True,
-    'SESSION_COOKIE_HTTPONLY': True,
-    'PERMANENT_SESSION_LIFETIME': 3600 * 24,
-    'JWT_ACCESS_TOKEN_EXPIRES': timedelta(minutes=15),
-    'JWT_REFRESH_TOKEN_EXPIRES': timedelta(days=1),
-    'REMEMBER_ME_REFRESH_TOKEN_EXPIRES': timedelta(days=180),
-    'JWT_BLACKLIST_ENABLED': True,
-    'JWT_BLACKLIST_TOKEN_CHECKS': ['access', 'refresh'],
-    'PASSWORD_RESET_TOKEN_EXPIRES': timedelta(days=7),
-}
+class AppConfig:
+    DEBUG = False
+    SESSION_COOKIE_SECURE = True
+    SESSION_COOKIE_HTTPONLY = True
+    PERMANENT_SESSION_LIFETIME = 3600 * 24
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(minutes=15)
+    JWT_REFRESH_TOKEN_EXPIRES = timedelta(days=1)
+    REMEMBER_ME_REFRESH_TOKEN_EXPIRES = timedelta(days=180)
+    JWT_BLACKLIST_ENABLED = True
+    JWT_BLACKLIST_TOKEN_CHECKS = ['access', 'refresh']
+    PASSWORD_RESET_TOKEN_EXPIRES = timedelta(days=7)
