d44cb41060e66d6754e9146a03c57ddf92578b00
Alex Ashley <alex@ashley-family.net>
2020-05-23 14:43:29 +0100

    update to use app.create_app()

--- application.py
+++ application.py
@@ -1,6 +1,7 @@
-
 import os, sys
 
 sys.path.append(os.path.abspath(os.path.dirname(__file__)))
 
-from musicbingo.server.app import app
+from musicbingo.server.app import create_app
+
+app = create_app()
