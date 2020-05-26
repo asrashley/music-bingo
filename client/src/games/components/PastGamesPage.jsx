d3d2071d9a6d51796f8d739ef3315abbf6295348
Alex Ashley <alex@ashley-family.net>
2020-05-26 09:05:22 +0100

    add vertical bar chart option for popularity graph
    The horizontal graph takes up quite a lot of space on the page.
    A vertical one is more compact.

--- client/src/games/components/PastGamesPage.jsx
+++ client/src/games/components/PastGamesPage.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
 import { BingoGamesTable } from './BingoGamesTable';
-import { initialState } from '../../app/initialState';
+import { PopularityGraph } from './PopularityGraph';
 
 import { fetchUserIfNeeded } from '../../user/userSlice';
 import { fetchGamesIfNeeded, invalidateGames } from '../gamesSlice';
@@ -12,48 +12,10 @@ import { getLocation } from '../../routes/selectors';
 import { getPastGamesList, getPastGamesPopularity } from '../gamesSelectors';
 import { getUser } from '../../user/userSelectors';
 
+import { initialState } from '../../app/initialState';
+
+
 import '../styles/games.scss';
-
-const colours = [
-  '#FF0000',
-  '#008000',
-  '#00EFEF',
-  '#808000',
-  '#BFBF00',
-  '#808080',
-  '#008080',
-  '#00EF00',
-  '#C0C0C0',
-  '#800000',
-  '#0000FF',
-  '#000080',
-  '#FF00FF',
-  '#800080',
-];
-
-function ThemeRow({index, theme}) {
-  const style = {
-    width: `${100 * theme.count / theme.maxCount}%`,
-    backgroundColor: colours[index % colours.length],
-  };
-  return (
-    <tr className="theme">
-      <td className="title" style={{color: colours[index % colours.length]}}>{theme.title}</td>
-      <td className="count"><div className="bar" style={style}>{theme.count}</div> </td>
-    </tr>
-  );
-}
-
-function PopularityGraph({popularity}) {
-  return (
-    <table className="popularity-graph">
-      <thead><tr><th className="title">Theme</th><th className="count">Popularity</th></tr></thead>
-      <tbody>
-        {popularity.map((theme, idx) => <ThemeRow key={idx} index={idx} theme={theme} />)}
-      </tbody>
-    </table>
-  );
-}
 
 class PastGamesPage extends React.Component {
   static propTypes = {
