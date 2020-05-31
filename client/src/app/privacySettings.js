1b4d6d35154588d6d9ff98f92aff5afd87c64bff
Alex Ashley <alex@ashley-family.net>
2020-05-31 22:28:34 +0100

    put privacy policy settings in its own file

---
+++ client/src/app/privacySettings.js
@@ -0,0 +1,12 @@
+export const company = {
+  name: "Acme",
+  email: "privacy@email.address",
+  address: "123 Somewhere Street, My Town"
+};
+
+export const dataCenter = 'anywhere';
+
+export const informationCommissioner = {
+  link: "https://ico.org.uk/",
+  text: "ico.org.uk",
+};
