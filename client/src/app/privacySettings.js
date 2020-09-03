c957b935b01c9c3b52c229bfe2fc1a7060ce2650
Alex Ashley <alex@ashley-family.net>
2020-09-03 12:37:26 +0100

    service moved to eu-west-1 region

--- client/src/app/privacySettings.js
+++ client/src/app/privacySettings.js
@@ -4,7 +4,7 @@ export const company = {
   address: "29 Mill Street, Redhill, Surrey, RH1 6PA"
 };
 
-export const dataCenter = "East Coast USA";
+export const dataCenter = "Ireland";
 
 export const informationCommissioner = {
   link: "https://ico.org.uk/",
