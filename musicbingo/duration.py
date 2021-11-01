49023692505d2669f8eb9951fc4b2c7ff597fe37
Alex Ashley <alex@ashley-family.net>
2021-11-01 16:36:22 +0000

    fixup Python 3.9 error calling typing.SupportsInt constructor
    Python 3.9 does not allow the constructor of a protocol type
    to be called:
    
      TypeError('Protocols cannot be instantiated')
    
    This commit removes this call to the SupportsInt constructor

--- musicbingo/duration.py
+++ musicbingo/duration.py
@@ -9,7 +9,6 @@ class Duration(SupportsInt):
     """Duration of a song (in milliseconds)"""
 
     def __init__(self, value: Union[int, str]) -> None:
-        super().__init__()
         if isinstance(value, str):
             value = int(self.parse(value))
         self._value: int = value
