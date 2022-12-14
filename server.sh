982daca98e0ad0cee7b71f42ec309c14e608a99b
Alex Ashley <alex@ashley-family.net>
2022-12-14 14:56:32 +0000

    use correct FLASK_APP setting for staring server on Unix
    The FLASK_APP setting was incorrect, as it uses the create_app()
    function rather than defining an "app" variable in that file.

--- server.sh
+++ server.sh
@@ -2,7 +2,7 @@
 export LC_ALL=C.UTF-8
 export LANG=C.UTF-8
 export FLASK_ENV=development
-export FLASK_APP=musicbingo.server.app:app
+export FLASK_APP=musicbingo.server.app
 
 flask run --host=0.0.0.0
 #python -m musicbingo.server
