74c5a5d84a09c396bf3a0b4ff7c0d6defd4e3feb
Alex Ashley <alex@ashley-family.net>
2023-03-06 22:09:34 +0000

    refactor AdminTicketDialog into a pure function component

--- client/src/tickets/components/AdminTicketDialog.jsx
+++ client/src/tickets/components/AdminTicketDialog.jsx
@@ -10,46 +10,43 @@ import { GamePropType } from '../../games/types/Game';
 import { TicketPropType } from '../types/Ticket';
 import { UserPropType } from '../../user/types/User';
 
-export class AdminTicketDialog extends React.Component {
-  static propTypes = {
-    game: GamePropType.isRequired,
-    ticket: TicketPropType.isRequired,
-    user: UserPropType.isRequired,
-    usersMap: PropTypes.object.isRequired,
-    onCancel: PropTypes.func.isRequired,
-    onAdd: PropTypes.func.isRequired,
-    onRelease: PropTypes.func.isRequired,
-  };
-
-  render() {
-    const { ticket, game, user, usersMap, onAdd, onCancel, onRelease } = this.props;
-    let username = 'you';
-    if (ticket.user && ticket.user !== user.pk) {
-      username = usersMap[ticket.user].username;
-    }
-    const footer = (
-      <div>
-        <Link to={reverse(`${routes.viewTicket}`, { gameId: game.id, ticketPk: ticket.pk })}
-          className="btn btn-primary play-game">View Ticket</Link>
-        {ticket.user && <button className="btn btn-primary yes-button"
-          onClick={() => onRelease(ticket)} >Release Ticket</button>}
-        {ticket.user === null && <button className="btn btn-primary yes-button"
-          onClick={() => onAdd(ticket)} >Claim Ticket</button>}
-        <button className="btn btn-secondary cancel-button"
-          data-dismiss="modal" onClick={onCancel}>Cancel</button>
-      </div>
-    );
-    return (
-      <ModalDialog
-        className="confirm-select-ticket"
-        id="confirm-select"
-        onCancel={onCancel}
-        title={`Ticket ${ticket.number}`}
-        footer={footer}
-      >
-        <h3>Ticket {ticket.number}</h3>
-        {ticket.user && <p>This ticket is owned by {username}</p>}
-      </ModalDialog>
-    );
+export function AdminTicketDialog({ ticket, game, user, usersMap, onAdd, onCancel, onRelease }) {
+  let username = 'you';
+  if (ticket.user > 0 && ticket.user !== user.pk) {
+    username = usersMap[ticket.user].username;
   }
+  const footer = (
+    <div>
+      <Link to={reverse(`${routes.viewTicket}`, { gameId: game.id, ticketPk: ticket.pk })}
+        className="btn btn-primary play-game">View Ticket</Link>
+      {ticket.user && <button className="btn btn-primary yes-button"
+        onClick={() => onRelease(ticket)} >Release Ticket</button>}
+      {ticket.user <= 0 && <button className="btn btn-primary yes-button"
+        onClick={() => onAdd(ticket)} >Claim Ticket</button>}
+      <button className="btn btn-secondary cancel-button"
+        data-dismiss="modal" onClick={onCancel}>Cancel</button>
+    </div>
+  );
+  return (
+    <ModalDialog
+      className="confirm-select-ticket"
+      id="confirm-select"
+      onCancel={onCancel}
+      title={`Ticket ${ticket.number}`}
+      footer={footer}
+    >
+      <h3>Ticket {ticket.number}</h3>
+      {ticket.user > 0 && <p>This ticket is owned by {username}</p>}
+    </ModalDialog>
+  );
 }
+
+AdminTicketDialog.propTypes = {
+  game: GamePropType.isRequired,
+  ticket: TicketPropType.isRequired,
+  user: UserPropType.isRequired,
+  usersMap: PropTypes.object.isRequired,
+  onCancel: PropTypes.func.isRequired,
+  onAdd: PropTypes.func.isRequired,
+  onRelease: PropTypes.func.isRequired,
+};
