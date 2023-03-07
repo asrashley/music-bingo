50e6442d647bb47424065032e4b659c81a016db5
Alex Ashley <alex@ashley-family.net>
2023-03-07 21:48:16 +0000

    add unit test for LogoutDialog component

--- client/src/user/components/LogoutDialog.jsx
+++ client/src/user/components/LogoutDialog.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
 import { ModalDialog } from '../../components';
 
 export function LogoutDialog({ onCancel, onConfirm, backdrop }) {
   const footer = (
     <div>
-      <button type="submit" className="btn btn-success login-button" onClick={onCancel} >No, I want to stay!</ button>
-      <button type="submit" className="btn btn-danger login-button" onClick={onConfirm} >Yes, log out</ button>
+      <button type="button" className="btn btn-success login-button" onClick={onCancel} >No, I want to stay!</ button>
+      <button type="button" className="btn btn-danger login-button" onClick={onConfirm} >Yes, log out</ button>
     </div>
   );
 
@@ -22,3 +24,8 @@ export function LogoutDialog({ onCancel, onConfirm, backdrop }) {
     </div>
   );
 }
+LogoutDialog.propTypes = {
+  onCancel: PropTypes.func.isRequired,
+  onConfirm: PropTypes.func.isRequired,
+  backdrop: PropTypes.bool
+};
---
+++ client/src/user/components/LogoutDialog.test.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { fireEvent } from '@testing-library/react';
+
+import { renderWithProviders } from '../../testHelpers';
+import { LogoutDialog } from './LogoutDialog';
+
+describe('LogoutDialog component', () => {
+	it('calls onCancel() and onConfirm() when buttons are clicked', async () => {
+		const props = {
+			onCancel: jest.fn(),
+			onConfirm: jest.fn(),
+			backdrop: false
+		};
+
+		const result = renderWithProviders(
+			<LogoutDialog {...props} />);
+		result.getByText('Logout from Musical Bingo?');
+		fireEvent.click(result.getByText('Yes, log out'));
+		expect(props.onConfirm).toHaveBeenCalledTimes(1);
+		expect(props.onCancel).toHaveBeenCalledTimes(0);
+		fireEvent.click(result.getByText('No, I want to stay!'));
+		expect(props.onConfirm).toHaveBeenCalledTimes(1);
+		expect(props.onCancel).toHaveBeenCalledTimes(1);
+		expect(result.asFragment()).toMatchSnapshot();
+	});
+});
