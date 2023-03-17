f486b195ee724959be3813f35925c1b7deb1ea1b
Alex Ashley <alex@ashley-family.net>
2023-03-17 20:22:19 +0000

    add unit tests for routes selectors

---
+++ client/src/routes/selectors.test.js
@@ -0,0 +1,75 @@
+import { getLocation, getBreadcrumbs } from './selectors';
+import routes from './index';
+
+const testCases = {
+  '/': [
+    {
+      className: "breadcrumb-item active",
+      label: 'Home',
+      url: null,
+    }
+  ],
+  '/register': [
+    {
+      className: "breadcrumb-item",
+      label: "Home",
+      url: "/"
+    }, {
+      className: "breadcrumb-item active",
+      label: "Register",
+      url: null
+    }
+  ],
+  '/user/modify': [
+    {
+      className: "breadcrumb-item",
+      label: "Home",
+      url: "/"
+    }, {
+      className: "breadcrumb-item",
+      label: "User",
+      url: "/user"
+    }, {
+      className: "breadcrumb-item active",
+      label: "Modify",
+      url: null
+    }
+  ],
+  '/game/2023-03-01/23': [
+    {
+      className: "breadcrumb-item",
+      label: "Home",
+      url: "/"
+    }, {
+      className: "breadcrumb-item",
+      label: "Game",
+      url: "/game"
+    }, {
+      className: "breadcrumb-item",
+      label: "2023-03-01",
+      url: "/game/2023-03-01"
+    }, {
+      className: "breadcrumb-item active",
+      label: "23",
+      url: null
+    }
+  ]
+};
+
+describe('routes selectors', () => {
+  it('index route is defined', () => {
+    expect(routes.index).toBe('/');
+  });
+
+  it.each(Object.keys(testCases))('generates breadcrumb context for "%s"', (pathname) => {
+    const state = {
+      router: {
+        location: {
+          pathname
+        }
+      }
+    };
+    expect(getLocation(state)).toEqual({ pathname });
+    expect(getBreadcrumbs(state)).toEqual(testCases[pathname]);
+  });
+});
