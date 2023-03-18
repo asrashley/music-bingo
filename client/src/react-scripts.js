f35c7b643cea9c9cb127dc98014d120d7da90774
Alex Ashley <alex@ashley-family.net>
2023-03-18 10:24:35 +0000

    move tar file generation into react-scripts
    To allow the tar file generator to use information from git
    (such as branch name), move the generation of the tar file after
    the build into react-scripts.js.
    
    This means that the tar file produced by a build of the default
    branch has been renamed from musicbingo-prod to musicbingo-main.

--- client/src/react-scripts.js
+++ client/src/react-scripts.js
@@ -36,25 +36,60 @@ if (command !== "build" && command !== "start") {
   exit(1);
 }
 
-getGitSha().
-  then((gitInfo) => {
-    const now = new Date();
-    const buildEnv = {
-      ...process.env,
-      BABEL_ENV: 'production',
-      NODE_ENV: 'production',
-      REACT_APP_GIT_BRANCH: gitInfo.branch,
-      REACT_APP_GIT_SHA: gitInfo.commit.hash,
-      REACT_APP_GIT_SHORT_SHA: gitInfo.commit.shortHash,
-      REACT_APP_GIT_TAGS: gitInfo.tags,
-      REACT_APP_BUILD_DATE: now.toISOString()
-    };
-    const buildProcess = spawn("node", [`node_modules/react-scripts/scripts/${command}.js`], {
+function runCommand(cmd, args, env) {
+  return new Promise((resolve) => {
+    console.log(`${cmd} ${args.join(' ')}`);
+    const buildProcess = spawn(cmd, args, {
       options: 'inherit',
-      env: buildEnv,
+      env,
     });
-
-    buildProcess.stdout.on('data', (data) => console.log(`${data}`) );
-    buildProcess.stderr.on('data', (data) => console.error(`${data}`) );
-    buildProcess.on('close', (code) => exit(code));
+    buildProcess.stdout.on('data', (data) => console.log(`${data}`));
+    buildProcess.stderr.on('data', (data) => console.error(`${data}`));
+    buildProcess.on('close', (code) => resolve(code));
   });
+}
+
+function createTarFile(props) {
+  const { gitInfo, exitCode } = props;
+  if (exitCode !== 0 || command !== 'build') {
+    return Promise.resolve(props);
+  }
+  const tarfile = `../musicbingo-${gitInfo.branch}.tar.gz`;
+  const args = [
+    '-czf', tarfile, '-C', '..', '--exclude=*.pyc',
+    '--exclude=__pycache__', 'client/build', 'musicbingo',
+    'Extra-Files', 'appspec.yml', 'application.py',
+    'dev-requirements.txt', 'mysql-requirements.txt',
+    'requirements.txt'
+  ];
+  return runCommand('tar', args, process.env)
+    .then(code => ({
+      ...props,
+      exitCode: code
+    }));
+}
+
+function runReactScript(gitInfo) {
+  const now = new Date();
+  const buildEnv = {
+    ...process.env,
+    BABEL_ENV: 'production',
+    NODE_ENV: 'production',
+    REACT_APP_GIT_BRANCH: gitInfo.branch,
+    REACT_APP_GIT_SHA: gitInfo.commit.hash,
+    REACT_APP_GIT_SHORT_SHA: gitInfo.commit.shortHash,
+    REACT_APP_GIT_TAGS: gitInfo.tags,
+    REACT_APP_BUILD_DATE: now.toISOString()
+  };
+  return runCommand('node', [`node_modules/react-scripts/scripts/${command}.js`], buildEnv)
+    .then((exitCode) => ({
+      now,
+      gitInfo,
+      exitCode
+  }));
+}
+
+getGitSha()
+  .then(runReactScript)
+  .then(createTarFile)
+  .then(({ exitCode }) => exit(exitCode));
