fc29e3d9b8b6a413ae0d5e9f9994584a228e6f69
Alex Ashley <github@ashley-family.net>
2023-12-08 19:27:50 +0000

    add test of rendering backdrop

--- client/src/components/BusyDialog.test.jsx
+++ client/src/components/BusyDialog.test.jsx
@@ -4,14 +4,19 @@ import { fireEvent } from '@testing-library/react';
 import { BusyDialog } from './BusyDialog';
 
 describe('BusyDialog component', () => {
+  const props = {
+    onClose: jest.fn(),
+    title: 'Dialog Title',
+    text: '49%',
+    backdrop: false
+  };
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
   it('renders busy dialog', () => {
-    const props = {
-      onClose: jest.fn(),
-      title: 'Dialog Title',
-      text: '49%',
-      backdrop: false
-    };
-    const { getByText, asFragment } = renderWithProviders(<BusyDialog {...props}>
+    const { getByText, container, asFragment } = renderWithProviders(<BusyDialog {...props}>
       <div>
         <p>child elements</p>
       </div>
@@ -19,7 +24,15 @@ describe('BusyDialog component', () => {
     getByText(props.title);
     getByText(props.text);
     getByText('child elements');
+    expect(container.querySelector('.modal-backdrop')).toBeNull();
     expect(asFragment()).toMatchSnapshot();
+  });
+
+  it('renders backdrop', () => {
+    const { getByText, container } = renderWithProviders(<BusyDialog
+      title={props.title} onClose={props.onClose} backdrop />);
+    getByText(props.title);
+    expect(container.querySelector('.modal-backdrop')).not.toBeNull();
   });
 
   it('calls onClose when cancel is called', () => {
