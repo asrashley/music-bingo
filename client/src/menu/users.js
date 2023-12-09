9b977a11308a51e4c06a545338bb289416bc5698
Alex Ashley <github@ashley-family.net>
2023-12-09 08:46:07 +0000

    move generation of menu sub-items into its own directory
    Create a new src/menus directory that contains functions that
    generate each menu item that is a drop-down menu.

---
+++ client/src/menu/users.js
@@ -0,0 +1,47 @@
+import { reverse } from 'named-urls';
+
+import routes from '../routes';
+import { getUser } from '../user/userSelectors';
+
+const adminMenuItems = [{
+    title: 'Modify Users',
+    href: reverse(`${routes.listUsers}`),
+}, {
+    title: 'Modify Settings',
+    href: reverse(`${routes.settingsIndex}`),
+}, {
+    title: 'Guest links',
+    href: reverse(`${routes.guestLinks}`),
+}];
+
+const loggedInUserMenuItems = [{
+    title: 'User Profile',
+    href: reverse(`${routes.user}`),
+}, {
+    title: 'Change password or email address',
+    href: reverse(`${routes.changeUser}`),
+}, {
+    title: 'Log out',
+    href: reverse(`${routes.logout}`),
+}];
+
+const adminLoggedIn = [
+    ...adminMenuItems,
+    ...loggedInUserMenuItems,
+];
+
+const guestMenuItems = [{
+    title: 'Log In',
+    href: reverse(`${routes.login}`),
+}];
+
+export function getUserMenuItems(state, props) {
+    const user = getUser(state, props);
+    if (!user.loggedIn) {
+        return guestMenuItems;
+    }
+    if (user.groups.admin === true) {
+        return adminLoggedIn;
+    }
+    return loggedInUserMenuItems;
+}
