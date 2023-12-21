20954fccedbb2999864812fed6345f3f6e8f723d
Alex Ashley <github@ashley-family.net>
2023-12-21 19:52:47 +0000

    add test for UserPage reloading when user changes

--- client/src/user/components/UserPage.test.jsx
+++ client/src/user/components/UserPage.test.jsx
@@ -7,20 +7,33 @@ import { createStore } from '../../store/createStore';
 import { initialState } from '../../store/initialState';
 import { renderWithProviders, installFetchMocks } from '../../testHelpers';
 import { UserPage, UserPageComponent } from './UserPage';
+import { userSlice } from '../userSlice';
 import * as user from '../../fixtures/userState.json';
 
 describe('UserPage component', () => {
+  let apiMocks;
+
   beforeAll(() => {
     jest.useFakeTimers('modern');
     jest.setSystemTime(new Date('08 Feb 2023 10:12:00 GMT').getTime());
   });
 
+  beforeEach(() => {
+    apiMocks = installFetchMocks(fetchMock, { loggedIn: true });
+  });
+
   afterEach(() => {
     fetchMock.mockReset();
+    jest.clearAllMocks();
+    log.resetLevel();
+    apiMocks = null;
     log.resetLevel();
   });
 
-  afterAll(() => jest.useRealTimers());
+  afterAll(() => {
+    jest.useRealTimers();
+    jest.restoreAllMocks();
+  });
 
   it('renders without throwing an exception with initial state', () => {
     const store = createStore(initialState);
@@ -31,9 +44,38 @@ describe('UserPage component', () => {
       },
       user: initialState.user
     };
-    installFetchMocks(fetchMock, { loggedIn: false });
     const result = renderWithProviders(<UserPageComponent {...props} />);
     result.getByText('Log out');
+  });
+
+  it.only('updates settings once user has logged in', async () => {
+    const username = 'my.username';
+    const password = 'password!';
+    const store = createStore(initialState);
+    const history = {
+      push: jest.fn(),
+    };
+    apiMocks.addUser({
+      username,
+      password,
+      groups: ['users'],
+      pk: 12,
+    });
+    const { getByText } = renderWithProviders(<UserPage history={history} />, { store });
+    getByText('Log out');
+
+    store.dispatch(userSlice.actions.receiveUser({
+      timestamp: 123,
+      payload: {
+        username: 'my.username',
+        email: 'a.user',
+        groups: ['users'],
+        pk: 12,
+        options: user.options,
+      },
+    }));
+
+    await screen.findByText('my.username');
   });
 
   it('renders admin user actions', async () => {
@@ -44,7 +86,6 @@ describe('UserPage component', () => {
     const history = {
       push: jest.fn()
     };
-    installFetchMocks(fetchMock, { loggedIn: true });
     const { asFragment } = renderWithProviders(<UserPage history={history} />, { store });
     const actions = [
       'Modify Users',
@@ -59,5 +100,4 @@ describe('UserPage component', () => {
     await Promise.all(actions.map(item => screen.findByText(item)));
     expect(asFragment()).toMatchSnapshot();
   });
-
 });
