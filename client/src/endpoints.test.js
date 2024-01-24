751f6d4f36a38404a4a97be78680b3cde241e0b2
Alex Ashley <github@ashley-family.net>
2024-01-24 09:02:32 +0000

    add JSON API tests for all methods when not logged in

--- client/src/endpoints.test.js
+++ client/src/endpoints.test.js
@@ -1,10 +1,15 @@
 import log from 'loglevel';
-import { vi } from 'vitest';
+import { describe, vi } from 'vitest';
 
-import { fetchWithRetry, receiveStream } from './endpoints';
+import { api, fetchWithRetry, receiveStream } from './endpoints';
 import { jsonResponse, fetchMock } from '../tests';
+import { MockBingoServer } from '../tests/MockServer';
+import { createStore, initialState } from './store';
+import { userSlice, refreshAccessToken } from './user/userSlice';
 
-describe('endpoints', () => {
+import settings from '../tests/fixtures/settings.json';
+
+describe('endpoints - fetchWithRetry()', () => {
   beforeEach(() => {
     vi.useFakeTimers();
     vi.setSystemTime(new Date('08 Feb 2023 10:12:00 GMT').getTime());
@@ -146,6 +151,18 @@ describe('endpoints', () => {
       ok: false
     });
   });
+});
+
+describe('endpoints - receiveStream()', () => {
+  beforeEach(() => {
+    vi.useFakeTimers();
+    vi.setSystemTime(new Date('08 Feb 2023 10:12:00 GMT').getTime());
+  });
+
+  afterEach(() => {
+    fetchMock.mockReset();
+    log.resetLevel();
+  });
 
   it('can process a multipart stream', async () => {
     const actions = [];
@@ -222,5 +239,200 @@ describe('endpoints', () => {
       }
     }];
     expect(actions).toEqual(expected);
+  });
+});
+
+describe('endpoints - api', () => {
+  describe('not logged in', () => {
+    let dispatch, server, store;
+
+    beforeEach(() => {
+      server = new MockBingoServer(fetchMock);
+      store = createStore(initialState);
+      dispatch = store.dispatch;
+    });
+
+    afterEach(() => {
+      server.shutdown();
+    });
+
+    it('check user', async () => {
+      await expect(dispatch(api.getUserInfo({
+        rejectErrors: false,
+        before: userSlice.actions.requestUser,
+        failure: userSlice.actions.failedFetchUser,
+        success: userSlice.actions.receiveUser,
+      }))).resolves.toMatchObject({
+        payload: {
+          error: '401: Unauthorized',
+          method: 'GET',
+          status: 401,
+          url: '/api/user',
+        },
+      });
+    });
+
+    it('refresh access token using invalid refresh token', async () => {
+      await expect(dispatch(refreshAccessToken('invalidRefreshToken'))).resolves.toMatchObject({
+        headers: {
+          Authorization: "Bearer invalidRefreshToken",
+        },
+        payload: {
+          error: '401: Unauthorized',
+          method: 'POST',
+          status: 401,
+          url: '/api/refresh',
+        },
+      });
+    });
+
+    it('logout', async () => {
+      await expect(dispatch(api.logout())).resolves.toMatchObject({
+        method: 'DELETE',
+        payload: 'Logged out',
+      });
+    });
+
+    it('registerUser', async () => {
+      const username = 'username';
+      const email = 'a.user@unit.test';
+      const password = 'secret';
+      await expect(dispatch(api.registerUser({
+        body: {
+          username,
+          email,
+          password
+        },
+        noAccessToken: true,
+        rejectErrors: false,
+      }))).resolves.toMatchObject({
+        method: 'PUT',
+        payload: {
+          message: "Successfully registered",
+          success: true,
+          user: {
+            username,
+            email,
+            groups: ['users'],
+          }
+        }
+      });
+    });
+
+    it('checkUser', async () => {
+      await expect(dispatch(api.checkUser({
+        body: {
+          username: 'username',
+          email: 'a.user@unit.test'
+        }
+      }))).resolves.toMatchObject({
+        method: 'POST',
+        url: "/api/user/check",
+        payload: {
+          email: false,
+          username: false,
+        }
+      });
+    });
+
+    it('passwordReset', async () => {
+      await expect(dispatch(api.passwordReset({
+        body: {
+          email: 'a.user@unit.test'
+        }
+      }))).resolves.toMatchObject({
+        method: 'POST',
+        url: "/api/user/reset",
+        payload: {
+          email: 'a.user@unit.test',
+          success: true,
+        }
+      });
+    });
+
+    it('checkGuestToken with missing token', async () => {
+      await expect(dispatch(api.checkGuestToken({
+        body: {},
+        noAccessToken: true,
+        rejectErrors: false,
+      }))).resolves.toMatchObject({
+        method: 'POST',
+        url: "/api/user/guest",
+        payload: {
+          status: 400,
+        }
+      });
+    });
+
+    it('checkGuestToken', async () => {
+      await expect(dispatch(api.checkGuestToken({
+        body: {
+          token: 'guest.token',
+        },
+        noAccessToken: true,
+        rejectErrors: false,
+      }))).resolves.toMatchObject({
+        method: 'POST',
+        url: "/api/user/guest",
+        payload: {
+          success: false,
+        }
+      });
+    });
+
+    it('getSettings', async () => {
+      await expect(dispatch(api.getSettings())).resolves.toMatchObject({
+        method: 'GET',
+        url: '/api/settings',
+        payload: {
+          privacy: settings.privacy,
+        }
+      });
+    });
+
+    it('importGame', async () => {
+      await expect(dispatch(api.importGame({
+        body: {
+          filename: 'gameTracks.json',
+        },
+      }))).resolves.toMatchObject({
+        method: 'PUT',
+        url: '/api/games',
+        streaming: true,
+        status: 401,
+      });
+    })
+    it.each([
+      ['getUserInfo', api.getUserInfo],
+      ['modifyMyself', () => api.modifyMyself({ body: {} })],
+      ['getGuestLinks', api.getGuestLinks],
+      ['createGuestToken', api.createGuestToken],
+      ['deleteGuestToken', () => api.deleteGuestToken({ token: 'guestToken' })],
+      ['getUsersList', api.getUsersList],
+      ['modifyUsers', api.modifyUsers],
+      ['getGamesList', api.getGamesList],
+      ['getGameDetail', () => api.getGameDetail({ gamePk: 43 })],
+      ['modifyGame', () => api.modifyGame({ gamePk: 42 })],
+      ['deleteGame', () => api.deleteGame({ gamePk: 42 })],
+      ['exportGame', () => api.exportGame({ gamePk: 42 })],
+      ['claimCard', () => api.claimCard({ gamePk: 23, ticketPk: 45 })],
+      ['releaseCard', () => api.releaseCard({ gamePk: 23, ticketPk: 45 })],
+      ['getTicketsList', () => api.getTicketsList({ gamePk: 43 })],
+      ['getTicketsStatus', () => api.getTicketsStatus({ gamePk: 43 })],
+      ['fetchCard', () => api.fetchCard({ gamePk: 23, ticketPk: 45 })],
+      ['setCardCellChecked', () => api.setCardCellChecked({ gamePk: 12, ticketPk: 34, number: 5, checked: true })],
+      ['downloadCard', () => api.claimCard({ gamePk: 23, ticketPk: 45 })],
+      ['getDirectoryDetail', () => api.getDirectoryDetail({ dirPk: 12 })],
+      ['getDirectoryList', api.getDirectoryList],
+      ['searchForSongs', () => api.searchForSongs({ query: 'Pop', dirPk: 5 })],
+      ['modifySettings', () => api.modifySettings({ body: [] })],
+    ])('%s returns 401 status', async (_name, apiFn) => {
+      await expect(dispatch(apiFn())).resolves.toMatchObject({
+        payload: {
+          error: '401: Unauthorized',
+          status: 401,
+        },
+      });
+    });
   });
 });
