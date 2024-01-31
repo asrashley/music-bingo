01d68ad6a0f6fb0bf8d1ed6eceb3966355b2572c
Alex Ashley <github@ashley-family.net>
2024-01-31 21:01:10 +0000

    convert UserPage from class to function component

--- client/src/user/components/UserPage.jsx
+++ client/src/user/components/UserPage.jsx
@@ -1,7 +1,6 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import { connect } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { reverse } from 'named-urls';
 
 import { AdminActions } from '../../admin/components/AdminActions';
@@ -12,81 +11,62 @@ import { getUser } from '../../user/userSelectors';
 
 import { routes } from '../../routes/routes';
 
-import { UserPropType } from '../types/User';
-
 import '../styles/user.scss';
 
-export class UserPageComponent extends React.Component {
-  static propTypes = {
-    dispatch: PropTypes.func.isRequired,
-    user: UserPropType.isRequired,
-  };
+export function UserPage() {
+  const dispatch = useDispatch();
+  const user = useSelector(getUser);
 
-  componentDidMount() {
-    const { dispatch } = this.props;
+  useEffect(() => {
     dispatch(fetchUserIfNeeded());
-    dispatch(fetchSettingsIfNeeded());
-  }
+  }, [dispatch]);
 
-  componentDidUpdate(prevProps) {
-    const { dispatch, user } = this.props;
-    if (prevProps.user.pk !== user.pk) {
+  useEffect(() => {
+    if (user.loggedIn) {
       dispatch(fetchSettingsIfNeeded());
     }
-  }
+  }, [dispatch, user.loggedIn]);
 
-  render() {
-    const { user } = this.props;
-
-    return (
-      <div id="user-page">
-        <div className="user-details border border-secondary rounded">
-          <div className="form-group row">
-            <label htmlFor="username" className="col-sm-2 col-form-label field">Username</label>
-            <div className="col-sm-10">
-              <div className="form-control-plaintext value" id="username">
-                {user.username}
-              </div>
-            </div>
-          </div>
-          <div className="form-group row">
-            <label htmlFor="email" className="col-sm-2 col-form-label">Email</label>
-            <div className="col-sm-10">
-              <div className="form-control-plaintext" id="email">
-                {user.email}
-              </div>
+  return (
+    <div id="user-page">
+      <div className="user-details border border-secondary rounded">
+        <div className="form-group row">
+          <label htmlFor="username" className="col-sm-2 col-form-label field">Username</label>
+          <div className="col-sm-10">
+            <div className="form-control-plaintext value" id="username">
+              {user.username}
             </div>
           </div>
         </div>
-        <div className="user-commands">
-          <AdminActions buttonClassName="btn-lg mb-4" className='user-admin-actions' database>
-            <Link className="btn btn-lg btn-primary mb-4"
-              to={reverse(`${routes.listUsers}`)}>Modify Users
-            </Link>
-            <Link
-              className="btn btn-lg btn-primary mb-4"
-              to={reverse(`${routes.settingsIndex}`)}
-            >Modify Settings
-            </Link>
-            <Link className="btn btn-lg btn-primary mb-5"
-              to={reverse(`${routes.guestLinks}`)}>Guest links
-            </Link>
-          </AdminActions>
-          {user.guest.loggedIn !== true && <Link to={reverse(`${routes.changeUser}`)}
-            className="btn btn-lg btn-warning change-user mt-3 mb-5">
-            Change password or email address</Link>}
-          <Link to={reverse(`${routes.logout}`)}
-            className="btn btn-lg btn-primary logout mb-5">Log out</Link>
+        <div className="form-group row">
+          <label htmlFor="email" className="col-sm-2 col-form-label">Email</label>
+          <div className="col-sm-10">
+            <div className="form-control-plaintext" id="email">
+              {user.email}
+            </div>
+          </div>
         </div>
       </div>
-    );
-  }
+      <div className="user-commands">
+        <AdminActions buttonClassName="btn-lg mb-4" className='user-admin-actions' database>
+          <Link className="btn btn-lg btn-primary mb-4"
+            to={reverse(`${routes.listUsers}`)}>Modify Users
+          </Link>
+          <Link
+            className="btn btn-lg btn-primary mb-4"
+            to={reverse(`${routes.settingsIndex}`)}
+          >Modify Settings
+          </Link>
+          <Link className="btn btn-lg btn-primary mb-5"
+            to={reverse(`${routes.guestLinks}`)}>Guest links
+          </Link>
+        </AdminActions>
+        {user.guest.loggedIn !== true && <Link to={reverse(`${routes.changeUser}`)}
+          className="btn btn-lg btn-warning change-user mt-3 mb-5">
+          Change password or email address</Link>}
+        <Link to={reverse(`${routes.logout}`)}
+          className="btn btn-lg btn-primary logout mb-5">Log out</Link>
+      </div>
+    </div>
+  );
 }
-
-const mapStateToProps = (state, props) => {
-  return {
-    user: getUser(state, props)
-  };
-};
-
-export const UserPage = connect(mapStateToProps)(UserPageComponent);
